f5404f4ff68b9f3f7788287b82a96bdf160cc2b9
elsa.m <manangelsa07@gmail.com>
2024-03-26 10:46:21 +0100

    minor update

---
+++ TeamsUI/ViewModels/Components/SideBarViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TeamsUI.Models;
+
+namespace TeamsUI.ViewModels.Components
+{
+    public partial class SideBarViewModel : ViewModelBase
+    {
+        private ObservableCollection<Menu> _menus;
+        public string Title => "titre";
+
+        public ObservableCollection<Menu> Menus
+        {
+            get => _menus;
+            set => SetProperty(ref _menus, value);
+        }
+
+        public SideBarViewModel()
+        {
+            var data = new ObservableCollection<Menu>();
+            for (var i = 1; i <= 5; i++)
+            {
+                data.Add(new Menu { Name = $"menu{i}" });
+            }
+            _menus = data;
+    }
+    }
+}
